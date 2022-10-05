1d5e506b0c1675fa285d95781c5ca4ccf800cab8
Joel Ical <2005113038@ub.edu.bz>
2022-10-05 08:23:57 -0600

    created our routes.go to hold our all routes

---
+++ cmd/api/routes.go
@@ -0,0 +1,21 @@
+//Filename: cmd/api/routes.go
+
+package main
+
+import (
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// create a method that returns a http router
+func (app *application) routes() *httprouter.Router {
+	//Create a new httrouter router instance
+	router := httprouter.New()
+
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+	//router.HandlerFunc(http.MethodPost, "/v1/string", app.createStringHandler)
+	//router.HandlerFunc(http.MethodGet, "/v1/student", app.showStudentHandler)
+
+	return router
+}
