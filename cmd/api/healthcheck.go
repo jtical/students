86fda701cff78ed182a56c762b27d8b62abc0017
Joel Ical <2005113038@ub.edu.bz>
2022-10-05 08:12:34 -0600

    created a healthcheck handler

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,15 @@
+//Filename: cmd/api/healthcheck.go
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// create a healthcheck handler
+func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", app.config.env)
+	fmt.Fprintf(w, "version: %s\n", version)
+}
