ef762fdbb12fc4bf60b25ff8fb84992655bbd721
Joel Ical <2005113038@ub.edu.bz>
2022-10-07 08:11:18 -0600

    modified healthcheck to return in json format

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -3,13 +3,31 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 // create a healthcheck handler post
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+	//create a map to hold healthcheck data
+	//we can make it an enevlope as its a map
+	data := map[string]string{
+		"status":      "available",
+		"enviornment": app.config.env,
+		"version":     version,
+	}
+	//convert our map to a json object
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encountered a issue and could not complete the request", http.StatusInternalServerError)
+		return
+	}
+	//add a new line to make view on the terminal
+	js = append(js, '\n')
+	//specify that we will serve our response in JSON
+	w.Header().Set("Content-Type", "application/json")
+	//write the [] byte contain the JSON response body
+	w.Write(js)
+
 }
--- cmd/api/students.go
+++ cmd/api/students.go
@@ -14,10 +14,12 @@ func (app *application) createStudentHandler(w http.ResponseWriter, r *http.Requ
 
 // showStudentHandler for the GET /v1/Student endpoint
 func (app *application) showStudentHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Student_ID: 2005113038")
-	fmt.Fprintln(w, "Student_Name: Joel T. Ical")
-	fmt.Fprintln(w, "Hobby: Drawing")
-	fmt.Fprintln(w, "Favorite_Color: Black")
+	js := `{"Student_ID": "2005113038", "Student_Name": "Joel T. Ical", "Hobby": "Drawing", "Favorite_Color": "Black"}`
+	js = fmt.Sprintf(js)
+	//specify that we will serve our response in JSON
+	w.Header().Set("Content-Type", "application/json")
+	//write the JSON as the HTTP response body
+	w.Write([]byte(js))
 }
 
 // createStudentHandler for the GET /v1/string endpoint
