75288f153605be024f5acfc9fabac25eaaf2eda9
Joel Ical <2005113038@ub.edu.bz>
2022-10-07 14:21:51 -0600

    encoding response in JSON format

--- internal/data/students.go
+++ internal/data/students.go
@@ -9,12 +9,12 @@ import (
 // holds student informatiom
 // back tick character(struct tags) shows how the key should be formated
 type Student struct {
-	ID        string
-	CreatedAt time.Time
-	Name      string
-	Phone     string
-	Email     string
-	Hobby     []string
-	Color     string
-	Version   int32
+	ID        string    `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Phone     string    `json:"phone"`
+	Email     string    `json:"email,omitempty"`
+	Hobby     []string  `json:"hobby"`
+	Color     string    `json:"color"`
+	Version   int32     `json:"version"`
 }
